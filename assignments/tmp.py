d657929b112eca8b1c55c665c2d1da731054b7ff
胡心洁(Xinjie.H) <kosam0913@gmail.com>
2024-05-05 10:58:44 +0800

    update

--- assignments/tmp.py
+++ assignments/tmp.py
@@ -3,7 +3,12 @@ from pathlib import Path
 target_dir = r"D:\xhu_temp\DeepLearning\assignments\ml_operation_specialization"
 
 for file in Path(target_dir).rglob("*.*"):
-    # rename with lower cases
+    # remove the starting underscore
     file_name = file.name
-    new_name = f"_{file_name.lower()}"
-    file.rename(new_name)
+    if file_name.startswith("_"):
+        new_name = file_name[1:]
+        new_file = file.parent / new_name
+        file.rename(new_file)
+        print(f"Renamed {file} to {new_file}")
+    else:
+        print(f"Skipped {file}")
